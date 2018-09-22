b0317ffe187a916dc67c08ac5b0ed1bdb30cb244
Richard <richard.alan.herbert@gmail.com>
2018-09-22 11:32:53 -0500

    add loop

--- app/Main.hs
+++ app/Main.hs
@@ -2,7 +2,7 @@ module Main where
 
 import Data.List.Split
 import Control.Monad.State
-import Numeric.LinearAlgebra                         (col, disp, Matrix, R, rand, scale, toLists, tr, (><))
+import Numeric.LinearAlgebra                         (col, disp, Matrix, R, rand, scale, toLists, tr, Vector, (><))
 import Numeric.LinearAlgebra.HMatrix                 (mul, sumElements)
 import System.IO
 
@@ -15,14 +15,11 @@ meanMatrix inputs batchSize = (sumElements inputs) / (fromIntegral batchSize)
 variance :: [Double] -> Double -> Double
 variance inputs mean = sum [(x - mean)**2 | x <- inputs]
 
-mult :: Matrix R -> Matrix R -> Matrix R
-mult inputs weights = (inputs * weights)
-
 sigmoid :: Matrix R -> Matrix R
 sigmoid inputs = 1.0 / (1.0 + exp (-1 * inputs))
 
 forward :: Matrix R -> Matrix R -> Matrix R
-forward inputs weights = sigmoid (mult inputs weights)
+forward inputs weights = sigmoid (mul inputs weights)
 
 criterion :: Matrix R -> Matrix R -> Matrix R
 criterion targets logits = (-1 * targets) * (log logits) - (1 - targets) * (log $ 1 - logits)
@@ -31,7 +28,7 @@ loss :: Matrix R -> Matrix R -> Int -> Double
 loss targets logits batchSize = meanMatrix (criterion targets logits) batchSize
 
 getGradient :: Matrix R -> Matrix R -> Matrix R -> Int -> Matrix R
-getGradient inputs targets logits batchSize = (inputs * (logits - targets)) / (fromIntegral batchSize)
+getGradient inputs targets logits batchSize = (mul (tr inputs) (logits - targets)) / (fromIntegral batchSize)
 
 updateWeights :: Double -> Matrix R -> Matrix R -> Matrix R
 updateWeights learningRate gradient weights = (weights - (learningRate `scale` gradient))
@@ -55,10 +52,6 @@ getTarget row = fromIntegral $ read $ row !! 1
 readCSV :: String -> [[[Char]]]
 readCSV file = [splitOn "," x | x <- (lines file)]
 
--- train :: Matrix R -> Matrix R -> Matrix R -> Int -> Matrix R
--- train inputs weights targets batchSize = getGradient targets logits inputs batchSize where
---     logits = forward inputs weights
-
 getNewWeights :: Matrix R -> Matrix R -> Matrix R -> Int -> Matrix R
 getNewWeights inputs targets weights batchSize =
     updateWeights 0.001 (getGradient inputs targets (forward inputs weights) batchSize) weights
@@ -66,13 +59,14 @@ getNewWeights inputs targets weights batchSize =
 runTrain :: Matrix R -> Matrix R -> State (Matrix R) (Matrix R)
 runTrain inputs targets = do
     weights <- get
-    put (getNewWeights inputs targets weights 891)
+    forM_ [0..50] $ \e -> do
+        put (getNewWeights inputs targets weights 891)
     return weights
 
 main = do
     contents <- readFile "data/titanic/train.csv"
-    weights <- rand 1 2
+    weights <- rand 1 1
     inputs <- processGender $ tail (readCSV contents)
     targets <- processTargets $ tail (readCSV contents)
-    print $ execState (runTrain inputs targets) weights
+    print $ loss targets (execState (runTrain inputs targets) weights) 891
 
